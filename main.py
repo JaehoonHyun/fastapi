28a34a083ed492bc5e4bdb2c15644d3c5cf6a280
rival0605 <rival0605@pubg.com>
2021-03-19 08:32:54 +0900

    add fast api

---
+++ main.py
@@ -0,0 +1,43 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+class Item(BaseModel):
+    user_id: str
+    password: str
+
+class PatchItem(BaseModel):
+    user_id: Optional[str]
+    password: Optional[str]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World"}
+
+@app.post("/register")
+async def register_item(item: Item):
+    global dicted_item
+    dicted_item = dict(item)
+    dicted_item['success'] = True
+
+    return JSONResponse(dicted_item)
+
+@app.put("/update")
+async def update_item(item: Item):
+    dicted_item = {k:v for k, v in dict(item).items()}
+    dicted_item['success'] = True
+
+    return JSONResponse(dicted_item)
+
+@app.patch("/update")
+async def update_item_sub(item: PatchItem):
+    for k, v in dict(item).items():
+        if v :
+            dicted_item[k] = v
+    dicted_item['success'] = True
+
+    return JSONResponse(dicted_item)
